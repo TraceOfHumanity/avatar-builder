4cd2a384cda6449c27756f8eec3375177b9b1005
Alexander_Ovchar <azalexxx02@gmail.com>
2024-10-01 10:56:46 +0300

    add 3d model

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {OrbitControls} from "@react-three/drei";
+import {OrbitControls, SoftShadows} from "@react-three/drei";
 import {Canvas} from "@react-three/fiber";
 import React from "react";
 
@@ -7,9 +7,10 @@ function App() {
     <div className="h-screen w-screen">
       <Canvas
         camera={{
-          position: [3, 3, 10],
+          position: [3, 1, 3],
         }}
       >
+        <color attach="background" args={["#303030"]} />
         <OrbitControls />
         <ambientLight />
         <pointLight position={[4, 5, 3]} intensity={20} />
@@ -17,6 +18,7 @@ function App() {
           <boxGeometry args={[1, 1, 1]} />
           <meshStandardMaterial color="red" />
         </mesh>
+        {/* <SoftShadows /> */}
       </Canvas>
     </div>
   );
