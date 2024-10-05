8835aa80507907181a02999c41f07c7eee6bbc42
Alexander_Ovchar <azalexxx02@gmail.com>
2024-10-05 21:30:38 +0300

    add cat avatar

--- src/components/Avater.tsx
+++ src/components/Avater.tsx
@@ -1,28 +1,9 @@
 import {useAnimations, useGLTF} from "@react-three/drei";
-import {useRef} from "react";
 
 import * as THREE from "three";
 
 export const Avater = () => {
-  const group = useRef<THREE.Group>(null);
-  const {nodes, materials, animations} = useGLTF("/3dModels/ComplateCat.gltf");
-  const {actions} = useAnimations(animations, group);
+  const avatar = useGLTF("/3dModels/ComplateCat.glb");
 
-  return (
-    // <group ref={group} dispose={null}>
-    //   {/* <group name="Scene"> */}
-    //   <group name="Cat">
-    //     <skinnedMesh
-    //       name="cat"
-    //       // geometry={nodes.cat.geometry}
-    //       // material={materials.cat}
-    //       // skeleton={nodes.cat.skeleton}
-    //     />
-    //   </group>
-    //   {/* </group> */}
-    // </group>
-    <group position={[0, -1, 0]}>
-      <primitive object={avatar.scene} />
-    </group>
-  );
+  return <group>{avatar && <primitive object={avatar.scene} />}</group>;
 };
