73136f886b1764be192b8684acf5d5149c7b61df
Alexander_Ovchar <azalexxx02@gmail.com>
2024-10-14 20:58:15 +0300

    delete all avatars

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import {OrbitControls} from "@react-three/drei";
 import {Canvas} from "@react-three/fiber";
 
 import {Menu} from "components";
-import { Avatar } from "components/3DModels/Avatar";
+import {Avatar} from "components/3DModels/Avatar";
 import {GirlCat2} from "components/3DModels/GirlCat2";
 
 function App() {
@@ -17,6 +17,8 @@ function App() {
         <color attach="background" args={["#303030"]} />
         <OrbitControls />
         <ambientLight intensity={10} />
+        {/* <directionalLight color="white" position={[0, 0, 5]} /> */}
+        {/* <directionalLight color="white" position={[2, 0, -3]} /> */}
         <pointLight position={[4, 5, 3]} intensity={100} />
         <mesh>
           <boxGeometry args={[1, 1, 1]} />
