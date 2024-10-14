8049eed694816222c43f5730c17b21e64712b6b7
Alexander_Ovchar <azalexxx02@gmail.com>
2024-10-14 21:29:51 +0300

    add xCorundum

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,17 @@ function App() {
       <Menu />
       <Canvas
         camera={{
-          position: [0, 0, 3],
+          position: [0, 1, 4],
         }}
       >
         <color attach="background" args={["#303030"]} />
         <OrbitControls />
         <ambientLight intensity={10} />
+        <directionalLight color="white" position={[2, 5, 5]} />
+        <directionalLight color="white" position={[2, 5, -5]} />
         {/* <directionalLight color="white" position={[0, 0, 5]} /> */}
         {/* <directionalLight color="white" position={[2, 0, -3]} /> */}
-        <pointLight position={[4, 5, 3]} intensity={100} />
+        {/* <pointLight position={[4, 5, 3]} intensity={100} /> */}
         {/* <mesh>
           <boxGeometry args={[1, 1, 1]} />
           <meshStandardMaterial color="red" />
