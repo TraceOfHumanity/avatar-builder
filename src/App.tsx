41f2bbaaa297445d188637e6518f94955e04ab92
Alexander_Ovchar <azalexxx02@gmail.com>
2024-10-21 21:50:30 +0300

    update styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import {Canvas} from "@react-three/fiber";
 
 import {Menu} from "components";
 import {Box} from "components/3DModels/Box";
+import { Untitled1 } from "components/3DModels/Untitled1";
 import {XCorundum} from "components/3DModels/XCorundum";
-import { XCorundum2 } from "components/3DModels/XCorundum2";
-import { XCorundum3 } from "components/3DModels/XCorundum3";
-import { XCorundum4 } from "components/3DModels/XCorundum4";
-import { XCorundum5 } from "components/3DModels/XCorundum5";
-import { YCorundum } from "components/3DModels/YCorundum";
+import {XCorundum2} from "components/3DModels/XCorundum2";
+import {XCorundum3} from "components/3DModels/XCorundum3";
+import {XCorundum4} from "components/3DModels/XCorundum4";
+import {XCorundum5} from "components/3DModels/XCorundum5";
+import {YCorundum} from "components/3DModels/YCorundum";
 
 function App() {
   return (
@@ -21,14 +22,15 @@ function App() {
       >
         <color attach="background" args={["#303030"]} />
         <OrbitControls />
-        <ambientLight intensity={0.8} />
+        <ambientLight intensity={0.5} />
         <pointLight position={[0, 10, 1]} intensity={200} />
         {/* <XCorundum /> */}
         {/* <XCorundum2 /> */}
         {/* <XCorundum3 /> */}
         {/* <XCorundum4 /> */}
         <XCorundum5 />
-        <YCorundum />
+        {/* <YCorundum /> */}
+        <Untitled1 />
       </Canvas>
     </div>
   );
