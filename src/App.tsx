946e696e9a9e06c7901bc87b01b3e553abd59b18
Alexander_Ovchar <azalexxx02@gmail.com>
2024-10-22 09:18:58 +0300

    delete 3d models

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,6 @@ import {OrbitControls} from "@react-three/drei";
 import {Canvas} from "@react-three/fiber";
 
 import {Menu} from "components";
-import {Box} from "components/3DModels/Box";
-import { Untitled1 } from "components/3DModels/Untitled1";
-import {XCorundum} from "components/3DModels/XCorundum";
-import {XCorundum2} from "components/3DModels/XCorundum2";
-import {XCorundum3} from "components/3DModels/XCorundum3";
-import {XCorundum4} from "components/3DModels/XCorundum4";
-import {XCorundum5} from "components/3DModels/XCorundum5";
-import {YCorundum} from "components/3DModels/YCorundum";
 
 function App() {
   return (
@@ -24,13 +16,7 @@ function App() {
         <OrbitControls />
         <ambientLight intensity={0.5} />
         <pointLight position={[0, 10, 1]} intensity={100} />
-        {/* <XCorundum /> */}
-        {/* <XCorundum2 /> */}
-        {/* <XCorundum3 /> */}
-        {/* <XCorundum4 /> */}
-        <XCorundum5 />
-        {/* <YCorundum /> */}
-        <Untitled1 />
+
       </Canvas>
     </div>
   );
